ea21818e53d75380f5d4176f4d334fbf35538fad
worldofMayur <mayurgatti7@gmail.com>
2026-05-30 11:59:13 +0530

    updated frontend logs

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-    base:"/TailAdmin/",
+  base: "/",           // ← Change this to root
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
