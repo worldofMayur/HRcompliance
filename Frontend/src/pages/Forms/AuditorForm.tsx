2cb4a98da4068ffdd06fd7f9c8e6825b93de244d
worldofMayur <mayurgatti7@gmail.com>
2026-06-23 11:28:56 +0530

    Disable vendccpffjfsghvhsvvjjccjxxvddgjkkxllpjopkhjhdospkgnkisssvvsxffxxxxshhcvdgljkjghbxxolxkoispxxooixisisxoslsxxjspskxosxosxkssmxssjssnsssxsdxsxixxsxsdFxxdxsxdxsxsxsxxsxs

--- Frontend/src/pages/Forms/AuditorForm.tsx
+++ Frontend/src/pages/Forms/AuditorForm.tsx
@@ -67,13 +67,6 @@ const formatDate = (dateString: string) => {
   });
 };
 
-const formatForAPI = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
 
 /* =========================
    COMPONENT
