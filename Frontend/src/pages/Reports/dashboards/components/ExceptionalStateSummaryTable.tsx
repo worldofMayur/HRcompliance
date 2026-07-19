63f58eb276daa9c34948174f4a69c5bb8ef87acd
worldofMayur <mayurgatti7@gmail.com>
2026-07-19 13:45:24 +0530

    Add VendorCompliancePayroll model for monthly payroll supportamkamakalmaamkalazazlaakzmaolaamzkamkakzm

--- Frontend/src/pages/Reports/dashboards/components/ExceptionalStateSummaryTable.tsx
+++ Frontend/src/pages/Reports/dashboards/components/ExceptionalStateSummaryTable.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Card, Table } from "antd";
 import type { ColumnsType } from "antd/es/table";
-import api from "../../../utils/api";
+import api from "../../../../utils/api";
 
 interface ExceptionalStateData {
   key: string;
