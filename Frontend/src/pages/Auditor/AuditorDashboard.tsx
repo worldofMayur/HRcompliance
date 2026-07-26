538c086edf506f823053a6eacf52b37a48b70e02
worldofMayur <mayurgatti7@gmail.com>
2026-07-26 14:31:22 +0530

    Add VendorCompliancePayroll model for monthly payroll supportamkaamzjnaamzjnaamzjakmzjaamzjaamzjamzjkaazjanaamzj

--- Frontend/src/pages/Auditor/AuditorDashboard.tsx
+++ Frontend/src/pages/Auditor/AuditorDashboard.tsx
@@ -4,6 +4,11 @@ import axios from "axios";
 import { Table, Input, Button, message, Modal, Tooltip } from "antd";
 import { Upload } from "antd";
 import { DownloadOutlined, SyncOutlined, UploadOutlined } from "@ant-design/icons";
+import InputField from "../components/form/input/InputField";
+import Label from "../components/form/Label";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 const API_BASE = import.meta.env.VITE_API_URL;
 export default function AuditorDashboard() {
   const token = localStorage.getItem("access_token");
